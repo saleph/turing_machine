f171516b5e19463302c8c3ab1cff0e6a07e80ac3
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-01-04 19:43:43 +0100

    [CHANGE] all exceptions moved to TMExceptions.h

---
+++ include/TMExceptions.h
@@ -0,0 +1,27 @@
+#ifndef TMEXCEPTIONS_H_INCLUDED
+#define TMEXCEPTIONS_H_INCLUDED
+
+#include <stdexcept>
+using std::invalid_argument;
+
+class MismatchCommandAndElementUnderHead : public invalid_argument {
+    public:
+        MismatchCommandAndElementUnderHead() : invalid_argument ( "Mismatch of command and the element under the head!" ) {};
+};
+
+class ZeroLongTape : public invalid_argument {
+    public:
+        ZeroLongTape() : invalid_argument ( "The tape cannot be zero-element-long!" ) {}
+};
+
+class HeadOutOfTape : public invalid_argument {
+    public:
+        HeadOutOfTape() : invalid_argument ( "The head position has cannot be larger than tapeSize - 1!" ) {}
+};
+
+class CharacterOutOfAlphabet : invalid_argument {
+    public:
+        CharacterOutOfAlphabet() : invalid_argument ( "Character out of defined alphabet!" ) {}
+};
+
+#endif // TMEXCEPTIONS_H_INCLUDED
