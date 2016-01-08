05473aa77f8107206ea9dd6bb3635aab10373857
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-01-08 22:58:33 +0100

    [ADD] input input interface

--- include/TMInputiInterface.h
+++ include/TMInputiInterface.h
@@ -1,13 +1,25 @@
 #ifndef TMINPUTINTERFACE_H
 #define TMINPUTINTERFACE_H
 
+#include <string>
+using std::string;
+
+
+struct TMContent {
+    string rawAlphabet;
+    string rawControlGraph;
+};
+
 
 class TMInputInterface
 {
     public:
-        virtual ~TMInputInterface() {};
-        virtual
-    private:
+        virtual ~TMInputInterface() {}
+        virtual void setContent(string alphabet, string controlGraph) = 0;
+        string getRawAlphabet() { return content.rawAlphabet; }
+        string getRawControlGraph() { return content.rawControlGraph; }
+    protected:
+        TMContent content;
 };
 
 #endif // TMINPUTINTERFACE_H
