8549b70c3fed4587e5ec5f7ef9ca77e45ea1c3fa
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-02-11 23:17:32 +0100

    [ADD] regexes of file parser

---
+++ include/TMFileParser.h
@@ -0,0 +1,49 @@
+#ifndef TMFILEPARSER_H
+#define TMFILEPARSER_H
+
+#include <fstream>
+#include <regex>
+
+
+class TMFileParser
+{
+    const std::regex commentPattern = std::regex (
+    "[\\/]{2}" // 2 slashes at the start of comment
+    ".*"       // content of the comment
+    );
+    const std::regex alphabetPattern = std::regex (
+    "^"        // start of line
+    "("        // start of matched group
+    "[\\/]?"   // first, optional slash as first char of alphabet
+    "[^\\/]"   // but if occurs second slash - DON'T match regexp
+    ".+"       // the rest of alphabet
+    ")"        // the end of matched group
+    );
+    const std::regex headPositionPattern = std::regex (
+    "^"        // start of line
+    "("        // start of matched group
+    "[^\\/]"   // but if occurs slash - DON'T match regexp
+    "[0-9]+"   // head position as number
+    ")"        // the end of matched group
+    );
+    const std::regex tapeContentPattern = std::regex (
+    "^"        // start of line
+    "([0-9]+)" // where tape content should begin
+    ";"        // semicolon separating position and the content
+    "(.*)"     // the content of the tape
+    );
+    const std::regex graphPattern = std::regex (
+    "^"        // start of line
+    "("        // start of matched group
+    "[\\/]?"   // first, optional slash, the at the start of command name
+    "[^\\/]"   // but if occurs second slash - DON'T match regexp
+    ".+"       // rest of the command's body
+    ")"        // the end of matched group
+    );
+    public:
+        TMFileParser() = default;
+    protected:
+    private:
+};
+
+#endif // TMFILEPARSER_H
---
+++ src/TMFileParser.cpp
@@ -0,0 +1,3 @@
+#include "TMFileParser.h"
+
+
