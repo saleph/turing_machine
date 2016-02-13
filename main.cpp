f9918ac20734d136a74e4b2b80678c3b78e7472b
Filip Konieczny <filkonieczny@gmail.com>
2016-02-13 21:44:20 +0100

    Fixed typo in compileInstertedGraph

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     TMAPI api(11);
     api.insertAlphabet("#10");
     api.insertGraph({"Start #/1;L next", "next #/1;L next2", "next2 #/0;L Stop"});
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     api.executeGraphInstantly();
     for (auto sign : *api.tape) cout<<sign;
     return 0;
