207aff69de3b0c13f7ad036eefb24bcf7ecf85a4
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-02-14 18:07:47 +0100

    [FIX] misspelling from API class

--- TuringMachineTests/UnitTests/TMAPITests.cpp
+++ TuringMachineTests/UnitTests/TMAPITests.cpp
@@ -17,7 +17,7 @@ BOOST_FIXTURE_TEST_SUITE(plain_API_for_11_long_tape, TMAPITestFixture)
 BOOST_AUTO_TEST_CASE( example_proper_use_with_instant_execution ) {
     api.insertAlphabet("#10");
     api.insertGraph({"Start #/1;L next", "next #/1;L next2", "next2 #/0;L Stop"});
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     api.executeGraphInstantly(); // ########### >> ###011#####
     // head position:                    ^           ^
     std::array<char, 11> outputTape = {'#', '#', '#', '0', '1', '1', '#', '#', '#', '#', '#'};
@@ -29,7 +29,7 @@ BOOST_AUTO_TEST_CASE( example_proper_use_with_setting_custom_HeadPositon_with_in
     api.insertAlphabet("#10");
     api.setHeadPosition(startHeadPosition - 1);
     api.insertGraph({"Start #/1;L next", "next #/1;L next2", "next2 #/0;L Stop"});
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     api.executeGraphInstantly(); // ########### >> ##011######
     // head position:                   ^           ^
     std::array<char, 11> outputTape = {'#', '#', '0', '1', '1', '#', '#', '#', '#', '#', '#'};
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_CASE( example_proper_use_with_setting_custom_HeadPositon_with_in
 BOOST_AUTO_TEST_CASE( doSingleStep_on_the_tape ) {
     api.insertAlphabet("#10");
     api.insertGraph({"Start #/1;L next", "next #/1;L next2", "next2 #/0;L Stop"});
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     api.doSingleStep(); // ########### >> #####1#####
     // head position:           ^             ^
     std::array<char, 11> outputTape = {'#', '#', '#', '#', '#', '1', '#', '#', '#', '#', '#'};
@@ -52,7 +52,7 @@ BOOST_AUTO_TEST_CASE( doSingleStep_on_the_tape_with_previously_set_headPosition 
     api.insertAlphabet("#10");
     api.setHeadPosition(startHeadPosition - 1);
     api.insertGraph({"Start #/1;L next", "next #/1;L next2", "next2 #/0;L Stop"});
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     api.doSingleStep(); // ########### >> ####1######
     // head position:          ^             ^
     std::array<char, 11> outputTape = {'#', '#', '#', '#', '1', '#', '#', '#', '#', '#', '#'};
@@ -62,7 +62,7 @@ BOOST_AUTO_TEST_CASE( doSingleStep_on_the_tape_with_previously_set_headPosition 
 
 BOOST_AUTO_TEST_CASE( execution_without_setting_alphabet ) {
     api.insertGraph({"Start #/1;L next", "next #/1;L next2", "next2 #/0;L Stop"});
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     BOOST_CHECK_THROW(api.doSingleStep(), CharacterOutOfAlphabet);
     BOOST_CHECK_THROW(api.executeGraphInstantly(), CharacterOutOfAlphabet);
     BOOST_CHECK_EQUAL(api.tape->getHeadPosition(), startHeadPosition);
@@ -78,7 +78,7 @@ BOOST_AUTO_TEST_CASE( execution_without_compilation_of_the_graph ) {
 
 BOOST_AUTO_TEST_CASE( execution_without_the_graph ) {
     api.insertAlphabet("#10");
-    api.compileInstertedGraph();
+    api.compileInsertedGraph();
     BOOST_CHECK_THROW(api.executeGraphInstantly(), CommandNotExist);
     BOOST_CHECK_THROW(api.doSingleStep(), CommandNotExist);
     BOOST_CHECK_EQUAL(api.tape->getHeadPosition(), startHeadPosition);
