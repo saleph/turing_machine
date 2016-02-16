c67369d34e65fcebf4a7b78bea0819796c4b2e2d
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-02-16 19:22:45 +0100

    [ADD] avoiding unneccessary reallocation

--- include/LazyInitializator.h
+++ include/LazyInitializator.h
@@ -10,7 +10,9 @@ class LazyInitializator : public std::unique_ptr<T> {
         LazyInitializator() : std::unique_ptr<T>() {} // this ctor offers lazy initialization
         LazyInitializator(const T& val) : std::unique_ptr<T> (std::make_unique<T>(val)) {} // for normal construction
         LazyInitializator& operator= (const T& val) { // assigning value which will be constructed into unique_ptr
-            std::unique_ptr<T>::operator= (std::make_unique<T>(val));
+            if (!std::unique_ptr<T>::operator bool()) // if object wasn't constructed
+                std::unique_ptr<T>::operator= (std::make_unique<T>(val)); // construct it
+            **this = val; // use of assingment provided by T - no unneccessary mem allocation
             return *this;
         }
 };
