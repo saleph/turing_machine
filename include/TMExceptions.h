3ec83a38c177103cb7be90a412d952e81971e388
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-02-23 22:44:13 +0100

    [ADD] clarified Exceptions' ctor

--- include/TMExceptions.h
+++ include/TMExceptions.h
@@ -5,79 +5,113 @@
 
 class TMException : public std::invalid_argument {
     public:
-        TMException(const std::string& arg) : std::invalid_argument (arg) {}
+        TMException(const std::string& arg)
+            : std::invalid_argument (arg)
+        {}
+        // ctor with clarification
+        TMException(const std::string& arg, const std::string& additional)
+            : std::invalid_argument ( arg + std::string(":\n") + additional )
+        {}
 };
 
 class MismatchCommandAndElementUnderHead : public TMException {
     public:
-        MismatchCommandAndElementUnderHead() : TMException ( "Mismatch of command and the element under the head!" ) {}
+        MismatchCommandAndElementUnderHead()
+            : TMException ( "Mismatch of command and the element under the head" )
+        {}
 };
 
 class ZeroLongTape : public TMException {
     public:
-        ZeroLongTape() : TMException ( "The tape cannot be zero-element-long!" ) {}
+        ZeroLongTape()
+            : TMException ( "The tape cannot be zero-element-long" )
+        {}
 };
 
 class HeadOutOfTape : public TMException {
     public:
-        HeadOutOfTape() : TMException ( "The head position has cannot be larger than tapeSize - 1!" ) {}
+        HeadOutOfTape()
+            : TMException ( "The head position has cannot be larger than tapeSize - 1" )
+        {}
 };
 
 class CharacterOutOfAlphabet : public TMException {
     public:
-        CharacterOutOfAlphabet() : TMException ( "Character out of defined alphabet!" ) {}
+        CharacterOutOfAlphabet(const char& arg)
+            : TMException ( "Character on tape out of defined alphabet", std::to_string(arg) )
+        {}
 };
 
 class InvalidCommandSyntax : public TMException {
     public:
-        InvalidCommandSyntax() : TMException ( "Invalid syntax of command!" ) {}
+        InvalidCommandSyntax(const std::string& cmd)
+            : TMException ( "Invalid syntax of command", cmd )
+        {}
 };
 
 class CommandAlreadyExist : public TMException {
     public:
-        CommandAlreadyExist() : TMException ( "Command with this transition already exist!" ) {}
+        CommandAlreadyExist(const std::string& cmd)
+            : TMException ( "Command with this transition already exist", cmd )
+        {}
 };
 
 class CommandNotExist : public TMException {
     public:
-        CommandNotExist() : TMException ( "Command with this transition doesn't exist!" ) {}
+        CommandNotExist(const char& arg)
+            : TMException ( "Command with this transition doesn't exist", std::to_string(arg) )
+        {}
 };
 
 class EndOfTheControlGraph : public TMException {
     public:
-        EndOfTheControlGraph() : TMException ( "End of the control graph (the machine reach state 'Stop'!" ) {}
+        EndOfTheControlGraph()
+            : TMException ( "End of the control graph (the machine reach state 'Stop')" )
+        {}
 };
 
 class DataFromFileDoesntInserted : public TMException {
     public:
-        DataFromFileDoesntInserted() : TMException ( "Data doesn't loaded properly!" ) {}
+        DataFromFileDoesntInserted()
+            : TMException ( "Data doesn't loaded properly" )
+        {}
 };
 
 // === FILE PARSING EXCEPTIONS ===
 
 class TMFileSyntaxException : public TMException {
     public:
-        TMFileSyntaxException(const std::string& arg) : TMException (arg) {}
+        TMFileSyntaxException(const std::string& arg)
+            : TMException ( std::string("File damaged: ") + arg )
+        {}
 };
 
 class TMInvalidAlphabetSyntax : public TMFileSyntaxException {
     public:
-        TMInvalidAlphabetSyntax() : TMFileSyntaxException ( "Invalid alphabet syntax!" ) {}
+        TMInvalidAlphabetSyntax()
+            : TMFileSyntaxException ( "invalid alphabet syntax" )
+        {}
 };
 
 class TMInvalidHeadPositionSyntax : public TMFileSyntaxException {
     public:
-        TMInvalidHeadPositionSyntax() : TMFileSyntaxException ( "Invalid head position syntax!" ) {}
+        TMInvalidHeadPositionSyntax()
+            : TMFileSyntaxException ( "invalid head position syntax" )
+        {}
 };
 
 class TMInvalidTapeContentSyntax : public TMFileSyntaxException {
     public:
-        TMInvalidTapeContentSyntax() : TMFileSyntaxException ( "Invalid tape content syntax!" ) {}
+        TMInvalidTapeContentSyntax()
+            : TMFileSyntaxException ( "invalid tape content syntax" )
+        {}
 };
 
 class TMInvalidControlGraphSyntax : public TMFileSyntaxException {
     public:
-        TMInvalidControlGraphSyntax() : TMFileSyntaxException ( "Invalid control graph syntax!" ) {}
+        TMInvalidControlGraphSyntax()
+            : TMFileSyntaxException ( "invalid control graph syntax" )
+        {}
 };
 
 #endif // TMEXCEPTIONS_H_INCLUDED
